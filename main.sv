7104745bc4ecaa843fa69038bddd2a46daefdbaf
Mabin Mathew <41275509+MabinMat@users.noreply.github.com>
2022-04-08 19:27:00 -0400

    Add files via upload

--- main.sv
+++ main.sv
@@ -3,7 +3,7 @@ module main(clk,btnLeft,btnRight,btnUp,btnDown,leftInd,rightInd,breaklight1,brea
 input clk,btnLeft,btnRight,btnUp,btnDown;
 output reg leftInd,rightInd,leftMotor1,leftMotor2,rightMotor1,rightMotor2,breaklight1,breaklight2 = 0;
 
-
+//headlights connected start to 5V as they are always on
 
 //leftInd = 0;
 //rightInd = 0;
@@ -11,31 +11,39 @@ output reg leftInd,rightInd,leftMotor1,leftMotor2,rightMotor1,rightMotor2,breakl
 //leftMotor = 0;
 //rightMotor = 0;
 
+//variable for clock 
 reg[31:0] count;
 parameter D = 32'd25000000;
 
 
 always @(posedge clk) begin
 
-
+	//moves vehicle forward
 	if(btnUp == 1) begin
+		//set indicators & breaklights to OFF
 		leftInd = 0;
 		rightInd = 0;
 		breaklight1 = 0;
 		breaklight2 = 0;
 		
+		//Provide the needed polarity for the H-bridge 
+			//for the motor to move forward
 		leftMotor1 = 1;
 		leftMotor2 = 0;
 		rightMotor1 = 0;
 		rightMotor2 = 1;
 	end
 	
+	//moves vehicle backward
 	else if(btnDown == 1) begin
+	//set indicators & breaklights to OFF
 		leftInd = 0;
 		rightInd = 0;
 		breaklight1 = 0;
 		breaklight2 = 0;
 		
+		//Provide the needed polarity for the H-bridge 
+			//for the motor to move backward
 		leftMotor1 = 0;
 		leftMotor2 = 1;
 		rightMotor1 = 1;
@@ -43,18 +51,24 @@ always @(posedge clk) begin
 
 	end
 	
+	//Turns vehicle Left
 	else if (btnLeft == 1) begin
+		//rest timer variable
 		count <=32'd0;
 		count <= count + 32'd1;
-
+		
+		//set breaklights to OFF
 		breaklight1 = 0;
 		breaklight2 = 0;
 		
+		//Provide the needed polarity for the H-bridge 
+		//for the vehicle to move left
 		leftMotor1 = 1;
 		leftMotor2 = 0;
 		rightMotor1 = 0;
 		rightMotor2 = 0;
 		
+		//blinked LED every half a second
 		if(count >=(D-1)) begin
 			count <=32'd0;
 			
@@ -68,19 +82,25 @@ always @(posedge clk) begin
 		
 	end
 	
+	//Turns vehicle right
 	else if(btnRight == 1) begin
+		//rest timer variable
 		count <=32'd0;		
 		count <= count + 32'd1;
 		
+		//set breaklights to OFF
+		breaklight1 = 0;
+		breaklight2 = 0;
+		
+		//Provide the needed polarity for the H-bridge 
+		//for the vehicle to move right		
 		rightMotor1 = 0;
 		rightMotor2 = 1;
 		leftMotor1 = 0;
 		leftMotor2 = 0;
-		breaklight1 = 0;
-		breaklight2 = 0;
 		
 	
-		
+		//blinked LED every half a second
 		if(count >=(D-1)) begin
 			count <=32'd0;
 			
@@ -93,15 +113,19 @@ always @(posedge clk) begin
 		
 		end
 		
+	//vehicle is Idle 
 	else begin
-	leftMotor1  = 0;
-	leftMotor2  = 0;
-	rightMotor1 = 0;
-	rightMotor2 = 0;
-	breaklight1 = 1;
-	breaklight2 = 1;
-	leftInd = 0;
-	rightInd = 0;
+		//turn off all motors
+		leftMotor1  = 0;
+		leftMotor2  = 0;
+		rightMotor1 = 0;
+		rightMotor2 = 0;
+		//turn break lights ON
+		breaklight1 = 1;
+		breaklight2 = 1;
+		//turn indicators ON
+		leftInd = 0;
+		rightInd = 0;
 	end
 
 end
